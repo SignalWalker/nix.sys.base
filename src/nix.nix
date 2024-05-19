2b521b2e2ab179528b2c35060b90bad7df72007a
Ash Walker <ashurstwalker@gmail.com>
2024-05-19 10:03:48 -0400

    ...

--- src/nix.nix
+++ src/nix.nix
@@ -56,12 +56,14 @@ in {
           "https://cache.ngi0.nixos.org"
           "https://nix-community.cachix.org"
           "https://nixpkgs-wayland.cachix.org"
+          "https://cache.lix.systems"
         ];
         trusted-public-keys = [
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
           "cache.ngi0.nixos.org-1:KqH5CBLNSyX184S9BKZJo1LxrxJ9ltnY2uAs5c/f1MA="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+          "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
         ];
       };
     };
